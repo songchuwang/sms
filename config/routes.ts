d0187cf2981abbb7f543b2a12fa6fa5e33328ce0
宋楚望 <songchuwang@songchuwangdeMacBook-Pro.local>
2024-10-11 17:50:59 +0800

    feat(模块): 添加了个很棒的功能

--- config/routes.ts
+++ config/routes.ts
@@ -8,14 +8,90 @@ export default [
   {
     path: '/admin',
     name: '短信管理',
-    icon: 'crown',
+    icon: 'mail',
     access: 'canAdmin',
     routes: [
       { path: '/admin', redirect: '/admin/sub-page' },
-      { path: '/admin/sub-page', name: '短信列表', component: './Admin' },
+      { path: '/admin/smsList', name: '短信列表', component: './SmsManage/SmsList' },
+      { path: '/admin/upAndDowm', name: '上下行', component: './SmsManage/UpAndDowm' },
+      // { name: '查询表格', icon: 'table', path: '/list', component: './TableList' },
     ],
   },
-  { name: '查询表格', icon: 'table', path: '/list', component: './TableList' },
+  {
+    name: '通讯录管理',
+    icon: 'book',
+    path: '/AddressBookManage',
+    component: './AddressBookManage',
+  },
+  {
+    path: '/admin',
+    name: '消费充值',
+    icon: 'crown',
+    access: 'canAdmin',
+    routes: [
+      {
+        path: '/admin/consumptionDetails',
+        name: '消费明细',
+        component: './ConsumerRecharge/ConsumptionDetails',
+      },
+      {
+        path: '/admin/accountRecharge',
+        name: '账户充值',
+        component: './ConsumerRecharge/AccountRecharge',
+      },
+    ],
+  },
+  {
+    path: '/admin',
+    name: '报表统计',
+    icon: 'table',
+    access: 'canAdmin',
+    routes: [
+      {
+        path: '/admin/rechargeStatistics',
+        name: '充值统计',
+        component: './ReportStatistics/RechargeStatistics',
+      },
+      {
+        path: '/admin/SRVStatistics',
+        name: '收发量统计',
+        component: './ReportStatistics/SRVStatistics',
+      },
+    ],
+  },
+  {
+    path: '/admin',
+    name: '账户管理',
+    icon: 'appstore',
+    access: 'canAdmin',
+    routes: [
+      {
+        path: '/admin/employeeAccount',
+        name: '充值统计',
+        component: './AccountManage/EmployeeAccount',
+      },
+      { path: '/admin/roles', name: '收发量统计', component: './AccountManage/RolesManage' },
+    ],
+  },
+  {
+    path: '/admin',
+    name: '系统设置',
+    icon: 'setting',
+    access: 'canAdmin',
+    routes: [
+      { path: '/admin/smsTemplate', name: '短信模板', component: './Setting/SmsTemplate' },
+      {
+        path: '/admin/enterpriseCertification',
+        name: '企业认证',
+        component: './Setting/EnterpriseCertification',
+      },
+      {
+        path: '/admin/signatureManagement',
+        name: '签名管理',
+        component: './Setting/SignatureManagement',
+      },
+    ],
+  },
   { path: '/', redirect: '/welcome' },
   { path: '*', layout: false, component: './404' },
 ];
