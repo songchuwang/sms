2d9be66b608bc220e2d4aabc347dd4c44ef1a96e
宋楚望 <songchuwang@songchuwangdeMacBook-Pro.local>
2024-10-14 15:43:21 +0800

    feat(模块): 添加通讯录菜单交互功能

--- src/pages/AddressBookManage/index.tsx
+++ src/pages/AddressBookManage/index.tsx
@@ -1,20 +1,49 @@
 import { addRule, removeRule, rule, updateRule } from '@/services/ant-design-pro/api';
-import { PlusOutlined } from '@ant-design/icons';
-import type { ActionType, ProColumns, ProDescriptionsItemProps } from '@ant-design/pro-components';
+import { EditOutlined, PlusOutlined } from '@ant-design/icons';
+import type { ActionType, ProColumns, ProDescriptionsActionType } from '@ant-design/pro-components';
 import {
   FooterToolbar,
+  GridContent,
   ModalForm,
   PageContainer,
   ProDescriptions,
+  ProFormRadio,
   ProFormText,
   ProFormTextArea,
   ProTable,
 } from '@ant-design/pro-components';
 import '@umijs/max';
-import { Button, Drawer, Input, message } from 'antd';
+import { Button, Card, Col, Drawer, Input, Menu, message, Row } from 'antd';
+import { createStyles } from 'antd-style';
 import React, { useRef, useState } from 'react';
 import type { FormValueType } from './components/UpdateForm';
 import UpdateForm from './components/UpdateForm';
+
+const useStyles = createStyles(({}) => {
+  return {
+    menuBox: {},
+    groupBox: {
+      display: 'flex',
+      flexDirection: 'column',
+      '.ant-menu': {
+        // background:'red',
+        'border-inline-end': 'none !important',
+      },
+    },
+    groupItem: {
+      display: 'flex',
+      flexDirection: 'row',
+      justifyContent: 'space-between',
+      marginTop: '10px',
+      span: {
+        fontSize: 18,
+      },
+    },
+    tabsCard: {
+      '.ant-card-body': { padding: 0 },
+    },
+  };
+});
 
 /**
  * @en-US Add node
@@ -83,12 +112,18 @@ const handleRemove = async (selectedRows: API.RuleListItem[]) => {
     return false;
   }
 };
+
 const TableList: React.FC = () => {
+  const actionDesRef = useRef<ProDescriptionsActionType>();
   /**
    * @en-US Pop-up window of new window
    * @zh-CN 新建窗口的弹窗
    *  */
   const [createModalOpen, handleModalOpen] = useState<boolean>(false);
+
+  const [examineModalOpen, handleExamineModalOpen] = useState<boolean>(false);
+
+  const [notesModalOpen, handleNotesModalOpen] = useState<boolean>(false);
   /**
    * @en-US The pop-up window of the distribution update window
    * @zh-CN 分布更新窗口的弹窗
@@ -218,15 +253,34 @@ const TableList: React.FC = () => {
         <a
           key="config"
           onClick={() => {
-            handleUpdateModalOpen(true);
+            handleExamineModalOpen(true);
             setCurrentRow(record);
           }}
         >
           审核
         </a>,
-        <a key="subscribeAlert" href="https://procomponents.ant.design/">
+        <a
+          onClick={() => {
+            // handleNotesModalOpen(true);
+            setShowDetail(true);
+            setCurrentRow(record);
+          }}
+          key="subscribeAlert"
+        >
           备注
         </a>,
+        // render: (dom, entity) => {
+        //   return (
+        //     <a
+        //       onClick={() => {
+        //         setCurrentRow(entity);
+        //         setShowDetail(true);
+        //       }}
+        //     >
+        //       {dom}
+        //     </a>
+        //   );
+        // },
       ],
     },
   ];
@@ -316,6 +370,108 @@ const TableList: React.FC = () => {
         />
         <ProFormTextArea width="md" name="desc" />
       </ModalForm>
+      <ModalForm
+        title={'短信审核'}
+        width="400px"
+        open={examineModalOpen}
+        onOpenChange={handleExamineModalOpen}
+        onFinish={async (value) => {
+          const success = await handleAdd(value as API.RuleListItem);
+          if (success) {
+            handleExamineModalOpen(false);
+            if (actionRef.current) {
+              actionRef.current.reload();
+            }
+          }
+        }}
+      >
+        <ProFormRadio.Group
+          name="type"
+          label={''}
+          options={[
+            {
+              value: '0',
+              label: '通过，允许发送短信',
+            },
+            {
+              value: '1',
+              label: '驳回，不允许发送短信',
+            },
+          ]}
+        />
+        {/* <ProFormText
+          rules={[
+            {
+              required: true,
+              message: '规则名称为必填项',
+            },
+          ]}
+          width="md"
+          name="name"
+        /> */}
+        <ProFormTextArea
+          width="md"
+          name="desc"
+          placeholder="请输入驳回原因"
+          rules={[
+            {
+              required: true,
+              message: '请输入驳回原因',
+            },
+          ]}
+        />
+      </ModalForm>
+      <ModalForm
+        title={'备注'}
+        width="400px"
+        open={notesModalOpen}
+        onOpenChange={handleNotesModalOpen}
+        // onFinish={async (value) => {
+        //   const success = await handleAdd(value as API.RuleListItem);
+        //   if (success) {
+        //     handleExamineModalOpen(false);
+        //     if (actionRef.current) {
+        //       actionRef.current.reload();
+        //     }
+        //   }
+        // }}
+      >
+        <ProFormRadio.Group
+          name="type"
+          label={''}
+          options={[
+            {
+              value: '0',
+              label: '通过，允许发送短信',
+            },
+            {
+              value: '1',
+              label: '驳回，不允许发送短信',
+            },
+          ]}
+        />
+        {/* <ProFormText
+          rules={[
+            {
+              required: true,
+              message: '规则名称为必填项',
+            },
+          ]}
+          width="md"
+          name="name"
+        /> */}
+        <ProFormTextArea
+          width="md"
+          name="desc"
+          placeholder="请输入驳回原因"
+          rules={[
+            {
+              required: true,
+              message: '请输入驳回原因',
+            },
+          ]}
+        />
+      </ModalForm>
       <UpdateForm
         onSubmit={async (value) => {
           const success = await handleUpdate(value);
@@ -346,7 +502,44 @@ const TableList: React.FC = () => {
         }}
         closable={false}
       >
-        {currentRow?.name && (
+        <ProDescriptions
+          actionRef={actionDesRef}
+          title="高级定义列表 request"
+          column={1}
+          request={async () => {
+            return Promise.resolve({
+              success: true,
+              data: {
+                id: '这是一段文本2',
+                date: '20200730',
+                money: '12121',
+                reason: '原因',
+                reason2: '原因2',
+              },
+            });
+          }}
+          // extra={<Button type="link">修改</Button>}
+        >
+          <ProDescriptions.Item dataIndex="id" label="审核人" />
+          <ProDescriptions.Item dataIndex="date" label="日期" valueType="date" />
+          <ProDescriptions.Item dataIndex="reason" label="审核未通过原因" />
+          <ProDescriptions.Item dataIndex="date" label="短信发送时间" />
+          <ProDescriptions.Item dataIndex="reason2" label="发送失败原因" />
+          <ProDescriptions.Item label="money" dataIndex="money" valueType="money" />
+          {/* <ProDescriptions.Item label="文本" valueType="option">
+            <Button
+              type="primary"
+              onClick={() => {
+                actionRef.current?.reload();
+              }}
+              key="reload"
+            >
+              刷新
+            </Button>
+            <Button key="rest">重置</Button>
+          </ProDescriptions.Item> */}
+        </ProDescriptions>
+        {/* {currentRow?.name && (
           <ProDescriptions<API.RuleListItem>
             column={2}
             title={currentRow?.name}
@@ -358,9 +551,187 @@ const TableList: React.FC = () => {
             }}
             columns={columns as ProDescriptionsItemProps<API.RuleListItem>[]}
           />
-        )}
+        )} */}
       </Drawer>
     </PageContainer>
   );
 };
-export default TableList;
+
+const Center: React.FC = () => {
+  const { styles } = useStyles();
+  const [createModalOpen, handleCreateModalOpen] = useState<boolean>(false);
+
+  const [modalTitle, handleUpdateModalTitle] = useState<string>('新建通讯录分组');
+
+  const items = [
+    {
+      key: 'grp',
+      label: '通讯录',
+      type: 'group',
+      children: [
+        {
+          key: '1',
+          label: (
+            <div
+              style={{
+                display: 'flex',
+                flexDirection: 'row',
+                alignItems: 'center',
+                justifyContent: 'space-between',
+                flex: 1,
+              }}
+            >
+              <span>Ant Design</span>
+              <EditOutlined
+                onClick={() => {
+                  handleCreateModalOpen(true);
+                  handleUpdateModalTitle('编辑通讯录分组');
+                }}
+                style={{ color: '#1890ff' }}
+              />
+            </div>
+          ),
+        },
+        {
+          key: '2',
+          label: (
+            <div
+              style={{
+                display: 'flex',
+                flexDirection: 'row',
+                alignItems: 'center',
+                justifyContent: 'space-between',
+                flex: 1,
+              }}
+            >
+              <span>Ant Design</span>
+              <EditOutlined style={{ color: '#1890ff' }} />
+            </div>
+          ),
+        },
+        {
+          key: '3',
+          label: (
+            <div
+              style={{
+                display: 'flex',
+                flexDirection: 'row',
+                alignItems: 'center',
+                justifyContent: 'space-between',
+                flex: 1,
+              }}
+            >
+              <span>Ant Design</span>
+              <EditOutlined style={{ color: '#1890ff' }} />
+            </div>
+          ),
+        },
+      ],
+    },
+  ];
+  return (
+    <GridContent>
+      <Row gutter={2}>
+        <Col lg={4} md={24}>
+          <Card
+            bordered={false}
+            style={{
+              marginBottom: 24,
+            }}
+            loading={false}
+          >
+            <div className={styles.groupBox}>
+              <Button
+                type="primary"
+                key="primary"
+                onClick={() => {
+                  handleCreateModalOpen(true);
+                  handleUpdateModalTitle('新建通讯录分组');
+                }}
+              >
+                <PlusOutlined /> 新建分组
+              </Button>
+              {/* <div className={styles.groupItem}>
+                <span>分组1(1000)</span>
+                <EditOutlined />
+              </div>
+              <div className={styles.groupItem}>
+                <span>分组1(1000)</span>
+                <EditOutlined />
+              </div>
+              <div className={styles.groupItem}>
+                <span>分组1(1000)</span>
+                <EditOutlined />
+              </div> */}
+              <Menu
+                // onClick={onClick}
+                // style={{
+                //   width: 256,
+                // }}
+                defaultSelectedKeys={['1']}
+                defaultOpenKeys={['sub1']}
+                mode="inline"
+                items={items}
+              />
+            </div>
+
+            {/* {!loading && currentUser && (
+              <div>
+                <Divider dashed />
+                <Divider
+                  style={{
+                    marginTop: 16,
+                  }}
+                  dashed
+                />
+              </div>
+            )} */}
+          </Card>
+        </Col>
+        <Col lg={20} md={24}>
+          <Card
+            className={styles.tabsCard}
+            // bordered={false}
+            // tabList={operationTabList}
+            // activeTabKey={tabKey}
+            // onTabChange={(_tabKey: string) => {
+            //   setTabKey(_tabKey as tabKeyType);
+            // }}
+          >
+            {/* <div>1111</div> */}
+            <TableList />
+          </Card>
+        </Col>
+      </Row>
+      <ModalForm
+        title={modalTitle}
+        width="400px"
+        open={createModalOpen}
+        onOpenChange={handleCreateModalOpen}
+        onFinish={async (value) => {
+          const success = await handleAdd(value as API.RuleListItem);
+          if (success) {
+            handleCreateModalOpen(false);
+            // if (actionRef.current) {
+            //   actionRef.current.reload();
+            // }
+          }
+        }}
+      >
+        <ProFormText
+          rules={[
+            {
+              required: true,
+              message: '规则名称为必填项',
+            },
+          ]}
+          width="md"
+          name="name"
+          label="分组名称"
+        />
+      </ModalForm>
+    </GridContent>
+  );
+};
+
+export default Center;
